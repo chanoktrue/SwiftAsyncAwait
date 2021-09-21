f4c52b30a537d6a9f33c05e2cdf04188afcce44d
Thongchai Subsaidee <chanoktrue@me.com>
2021-09-21 13:08:04 +0700

    start project

---
+++ SwiftAsyncAwait/SwiftAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAsyncAwaitApp.swift
+//  SwiftAsyncAwait
+//
+//  Created by Thongchai Subsaidee on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
